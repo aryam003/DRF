a133ef4021de262c21080b87daf2629cf56194b0
aryam003 <aryamarya537@gmail.com>
2024-11-29 22:25:25 +0530

    API_work

--- DRF/app/views.py
+++ DRF/app/views.py
@@ -63,3 +63,85 @@ def fun3(req):
         else:
             return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET','PUT','DELETE'])
+def fun4(req,id):
+    try:
+        demo=project_user.objects.get(pk=id)
+    except project_user.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if req.method=='GET':
+        s=model_serializer(demo)
+        return Response(s.data)
+    elif req.method=='PUT':
+        s=model_serializer(demo,data=req.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif req.method=='DELETE':
+        demo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class fun5(APIView):
+    def get(self,req):
+        demo=project_user.objects.all()
+        s=model_serializer(demo,many=True)
+        return Response(s.data)
+    def post(self,req):
+        s=model_serializer(data=req.data)
+        if s.is_valid():
+            s.save()
+            return JsonResponse(s.data,status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class fun6(APIView):
+    def get(self,req,id):
+        try:
+            demo=project_user.objects.get(pk=id)
+            s=model_serializer(demo)
+            return Response(s.data)
+        except project_user.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def put(sef,req,id):
+        try:
+            demo=project_user.objects.get(pk=id)
+            s=model_serializer(demo,data=req.data)
+            if s.is_valid():
+                s.save()
+                return Response(s.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except project_user.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def delete(self,req,id):
+        try:
+            demo=project_user.objects.get(pk=id)
+            demo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except project_user.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+class genericapiview(generics.GenericAPIView,mixins.ListModelMixin,mixins.creatModelMixin):
+    serializer_class=model_serializer
+    queryset=project_user.objects.all()
+    def get(self,req):
+        return self.list(req)
+    def post(self,req):
+        return self.create(req)
+    
+class update(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+    serializer_class=model_serializer
+    queryset=project_user.objects.all()
+    Lookup_field='id'
+    def get(self,req,id=None):
+        return self.retrieve(req,id)
+    def put(self,req,id=None):
+        return self.update(req,id)
+    def deletet(self,req,id):
+        return self.destroy(req,id)
+
+
+        
+        
+    
